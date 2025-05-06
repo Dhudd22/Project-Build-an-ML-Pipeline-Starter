e01ee1f43a14c0cb682814868ca179f0bc1699fd
Dhudd22 <dhudd22@wgu.edu>
2025-05-06 16:44:29 -0500

    once again

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -13,7 +13,6 @@ import json
 
 import pandas as pd
 import numpy as np
-from mlflow.models import infer_signature
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.impute import SimpleImputer
